773c56d31886923340bd298ab928faa2e9a24d69
Alshawesh <Alshawesh@ALSHAWESH>
2020-04-23 22:03:24 +0200

    Add project files.

---
+++ PassWord genrator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PassWord_genrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            this.bt_generate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_clear = new System.Windows.Forms.Button();
+            this.bt_copy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.Enabled = false;
+            this.tb_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_pass.ForeColor = System.Drawing.Color.BlueViolet;
+            this.tb_pass.Location = new System.Drawing.Point(12, 110);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.ReadOnly = true;
+            this.tb_pass.Size = new System.Drawing.Size(335, 30);
+            this.tb_pass.TabIndex = 0;
+            this.tb_pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bt_generate
+            // 
+            this.bt_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_generate.Location = new System.Drawing.Point(248, 165);
+            this.bt_generate.Name = "bt_generate";
+            this.bt_generate.Size = new System.Drawing.Size(94, 47);
+            this.bt_generate.TabIndex = 1;
+            this.bt_generate.Text = "Generate";
+            this.bt_generate.UseVisualStyleBackColor = true;
+            this.bt_generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Secure PassWord Generator";
+            // 
+            // bt_clear
+            // 
+            this.bt_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_clear.Location = new System.Drawing.Point(12, 165);
+            this.bt_clear.Name = "bt_clear";
+            this.bt_clear.Size = new System.Drawing.Size(94, 47);
+            this.bt_clear.TabIndex = 3;
+            this.bt_clear.Text = "Clear";
+            this.bt_clear.UseVisualStyleBackColor = true;
+            this.bt_clear.Click += new System.EventHandler(this.bt_clear_Click);
+            // 
+            // bt_copy
+            // 
+            this.bt_copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_copy.Location = new System.Drawing.Point(130, 165);
+            this.bt_copy.Name = "bt_copy";
+            this.bt_copy.Size = new System.Drawing.Size(94, 47);
+            this.bt_copy.TabIndex = 4;
+            this.bt_copy.Text = "Copy";
+            this.bt_copy.UseVisualStyleBackColor = true;
+            this.bt_copy.Click += new System.EventHandler(this.bt_copy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 267);
+            this.Controls.Add(this.bt_copy);
+            this.Controls.Add(this.bt_clear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_generate);
+            this.Controls.Add(this.tb_pass);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PassWord Genrator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_pass;
+        private System.Windows.Forms.Button bt_generate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_clear;
+        private System.Windows.Forms.Button bt_copy;
+    }
+}
+
---
+++ PassWord genrator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassWord_genrator
+{
+    /*This Code Its For Free Not For Sale 
+     DEV.Mohammed Alshawesh*/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_pass.Text = CreateRandomPassword();
+        }
+        public string CreateRandomPassword(int length = 20)
+        {
+            // Create a string of characters, numbers, special characters that allowed in the password  
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+
+            // Select one random character at a time from the string  
+            // and create an array of chars  
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+
+        }
+
+        private void bt_clear_Click(object sender, EventArgs e)
+        {
+            tb_pass.Clear();
+        }
+
+        private void bt_copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tb_pass.Text);
+        }
+    }
+}
